1296bda661d4c8ff4ea37a554d4425b62648dcef
sabtat132 <46466148+sabtat132@users.noreply.github.com>
2019-04-22 19:31:59 -0400

    Code for Each of the 5 Problems for the Final
    for 3 of the problems, they required a javascript (.js) file, so i decided to use the same .js file. I tried my best to really comment on that one in order to make it not confusing where each code for each project begins and ends

---
+++ finalscript.js
@@ -0,0 +1,39 @@
+
+//Problem #3
+function hideit(){
+  document.getElementById("ppu").className = "hidden";
+}
+
+//Problem #4
+function changeText(){
+  document.getElementById("p1").style.fontSize = "6em";
+  document.getElementById("p1").style.fontStyle= "Italic";
+  document.getElementById("p1").style.backgroundColor = "yellow";
+  document.getElementById("p1").style.fontWeight = "Bold";
+}
+
+//Problem #5
+  function alterText(){
+  document.getElementById("college").style.fontSize = "6em";
+  document.getElementById("college").style.fontStyle= "Italic";
+  document.getElementById("college").style.backgroundColor = "orange";
+  document.getElementById("college").style.fontWeight = "Bold";
+}
+
+
+function refresh(){
+  location.reload();
+}
+
+//i had time so i wanted to add more to #5
+function alterText2(){
+document.getElementById("college2").style.fontSize = "4em";
+document.getElementById("college2").style.fontStyle= "Underline";
+document.getElementById("college2").style.backgroundColor = "blue";
+document.getElementById("college2").style.fontWeight = "Thin";
+}
+
+
+function refresh2(){
+location.reload();
+}
